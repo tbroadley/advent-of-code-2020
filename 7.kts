399a3f0de57a4e42611b532784d9eecfcc29dc95
Thomas Broadley <thomas@faire.com>
2020-12-07 08:28:10 -0500

    Solve day 7 part 2

---
+++ 7.kts
@@ -0,0 +1,31 @@
+import java.io.File
+
+fun getBagCount(bagMap: Map<String, Map<String, Int>>, startingColour: String): Int {
+  val bags = bagMap[startingColour] ?: return 0
+  println(bags)
+
+  if (bags.isEmpty()) {
+    return 0
+  }
+
+  return bags.entries.asSequence()
+      .map { (colour, count) -> (getBagCount(bagMap, colour) + 1) * count }
+      .sum()
+}
+
+val input = File("7.txt").readLines()
+    .map { line ->
+      val (colour, bagCounts) = line.split(" bags contain ")
+      val bagCountMatches = "(\\d [a-z]+ [a-z]+) bags?".toRegex().findAll(bagCounts)
+          .map { match ->
+            val bagDescription = match.groupValues[1].split(" ")
+            val bagColour = bagDescription.drop(1).joinToString(" ")
+            val bagCount = bagDescription.first().toInt()
+            bagColour to bagCount
+          }
+          .toMap()
+      colour to bagCountMatches
+    }
+    .toMap()
+
+println(getBagCount(input, "shiny gold"))
